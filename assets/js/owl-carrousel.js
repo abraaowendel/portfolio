4541e0f762e85b13888266e9c356302b3249d1d9
Abraao Wendel <abraao.dev@gmail.com>
2022-09-08 21:15:49 -0300

    foi adicionado um carrousel com alguns projetos

---
+++ assets/js/owl-carrousel.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:true,
+        }
+    }
+})
